1a69501c788458e4d608737a1b0fee896c39bc57
Claus Due <claus@wildside.dk>
2012-10-14 00:30:34 +0200

    [FEATURE] Ajax Flux Content Loader
    Loads child content from a Flux-enabled content element's content areas
    through AJAX. Supports delay, requires jQuery and the static TypoScript
    for AJAX usage included in this extension.

---
+++ Classes/ViewHelpers/Content/AjaxFluxContentViewHelper.php
@@ -0,0 +1,47 @@
+<?php
+class Tx_Fluidwidget_ViewHelpers_Content_AjaxFluxContentViewHelper extends Tx_Fluidwidget_ViewHelpers_Request_DispatchViewHelper {
+
+	/**
+	 * @var Tx_Fluidwidget_Controller_SubRequestController
+	 */
+	protected $controller;
+
+	/**
+	 * @var string
+	 */
+	protected $tagName = 'section';
+
+	/**
+	 * @var boolean
+	 */
+	protected $ajaxWidget = TRUE;
+
+	/**
+	 * @param Tx_Fluidwidget_Controller_SubRequestController $controller
+	 */
+	public function injectController(Tx_Fluidwidget_Controller_SubRequestController $controller) {
+		$this->controller = $controller;
+	}
+
+	/**
+	 * Initialize arguments
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->registerArgument('parentUid', 'integer', 'UID of the parent content element', TRUE);
+		$this->registerArgument('area', 'string', 'Name of the area from which to render content', TRUE);
+		$this->overrideArgument('controller', 'string', 'Controller', FALSE, 'Flux');
+		$this->overrideArgument('pluginName', 'string', 'Plugin name', FALSE, 'API');
+		$this->overrideArgument('extensionName', 'string', 'Extension name', FALSE, 'Flux');
+		$this->overrideArgument('ajax', 'boolean', 'On by default; this is after all an Ajax Widget', FALSE, TRUE);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function render() {
+		$this->arguments['arguments']['parentUid'] = $this->arguments['parentUid'];
+		$this->arguments['arguments']['area'] = $this->arguments['are'];
+	}
+
+}
